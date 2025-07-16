6fc1a2f66087c169c72f721fff8f2a8f38c51782
lucas <amarallucas@id.uff.br>
2025-07-15 23:14:59 -0300

    Adiciona scroll

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
 
-transacoes = pd.read_csv("transacoes.csv", sep=';')
+mes_options = [i for i in range(1,13)]
+
+mes = st.selectbox("Mês", options=mes_options, index=datetime.today().month)
+
+transacoes = pd.read_csv(f"transacoes{mes}{datetime.today().year}.csv", sep=';', index_col=None)
 
 tab1, tab2, tab3 = st.tabs(["Compras", "Extrato", 'Visão Geral'])
 tab1.write("Adicionar compras")
@@ -12,11 +17,11 @@ tab2.write("Visão Geral")
 # You can also use "with" notation:
 with tab1:
 
-    tipo_options = ['Receita', 'Despesa']
+    tipo_options = ['Despesa', 'Receita']
     tipo_input = st.selectbox('Tipo', tipo_options)
 
     if tipo_input == 'Despesa':
-        area_options = ['Refeição', 'Lanche', 'Café', 'Supermercado', 'Utilidades', 'Lazer', 'Transporte']
+        area_options = ['Refeição', 'Bar', 'Lanche', 'Café', 'Vestuário', 'Supermercado', 'Utensílios', 'Lazer', 'Transporte', 'Lavanderia']
     elif tipo_input == 'Receita':
         area_options = ['Salário', 'Outro']
 
@@ -47,20 +52,50 @@ with tab1:
 
         transacoes = pd.concat([transacoes, new_row_df], ignore_index=True)
         st.write("Transação adicionada com sucesso!")
-        transacoes.to_csv('transacoes.csv', sep=';')
+        transacoes.to_csv(f'transacoes{mes}{datetime.today().year}.csv', sep=';', index=False)
 
 
 with tab2:
-    st.dataframe(transacoes)
+
+    if "transacoes" not in st.session_state:
+        st.session_state.df = transacoes
+
+        event = st.dataframe(
+            st.session_state.df,
+            key="data",
+            on_select="rerun",
+            selection_mode=["multi-row"],
+        )
+
+        #event.selection
+        #print(event.selection["rows"])
+
+        if st.button("Apagar transações marcadas"):
+            transacoes.drop(event.selection["rows"], inplace=True)
+            transacoes.to_csv(f'transacoes{mes}{datetime.today().year}.csv', sep=';', index=False)
+            st.session_state.df = transacoes
+
 
 with tab3:
     receita = transacoes[transacoes['tipo_input']=='Receita']['valor_input'].sum()
-    receita_str = f"# Receita {receita}"
+    receita_str = f"## Receita {receita}"
     st.markdown(receita_str)
 
     despesa = transacoes[transacoes['tipo_input']=='Despesa']['valor_input'].sum()
-    despesa_str = f"# Despesa {despesa}"
+    despesa_str = f"## Despesa {despesa}"
     st.markdown(despesa_str)
 
-    lucro_divida_str = f"# Lucro/Divida {receita - despesa}"
+    lucro_divida_str = f"## Lucro/Divida {receita - despesa}"
     st.markdown(lucro_divida_str)
+
+    group_despesa = transacoes[transacoes['tipo_input']=='Despesa'].groupby(by=['area_input'])['valor_input'].sum()
+    st.bar_chart(group_despesa)
+
+    group_despesa_local = transacoes[transacoes['tipo_input']=='Despesa'].groupby(by=['local_input'])['valor_input'].sum()
+    st.bar_chart(group_despesa_local)
+
+    group_receita = transacoes[transacoes['tipo_input']=='Receita'].groupby(by=['area_input'])['valor_input'].sum()
+    st.bar_chart(group_receita)
+
+    group_receita_local = transacoes[transacoes['tipo_input']=='Receita'].groupby(by=['local_input'])['valor_input'].sum()
+    st.bar_chart(group_receita_local)
